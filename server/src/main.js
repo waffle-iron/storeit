a9268a26404323c4abd6554f91ac1f7727dfcafd
Adrien morel <adrien.morel@me.com>
2016-05-17 06:31:14 -0700

    server: add basic command parsing

--- server/src/main.js
+++ server/src/main.js
@@ -1,7 +1,7 @@
 const commander = require('commander')
+const ws = require('./ws.js')
 
 commander.version('0.0.1')
   .option('-p', '--port <port>', 'set the port to listen to')
   .option('-a', '--addr <ip>', 'set the address to listen on')
   .parse(process.argv)
-  
