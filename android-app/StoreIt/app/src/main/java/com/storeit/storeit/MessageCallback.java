a9af0f4f1e3399d3287d5a32d9d9fa6024a81a0a
Louis Mondesir <louis.mondesir@gmail.com>
2016-05-17 14:01:09 +0900

    Android fix project

---
+++ android-app/StoreIt/app/src/main/java/com/storeit/storeit/MessageCallback.java
@@ -0,0 +1,9 @@
+package com.storeit.storeit;
+
+/**
+ * Created by loulo on 11/01/2016.
+ */
+public interface MessageCallback {
+
+    public void callbackMessageReceiver(String message);
+}
---
+++ android-app/StoreIt/app/src/main/java/com/storeit/storeit/protocol/LoginHandler.java
@@ -0,0 +1,8 @@
+package com.storeit.storeit.protocol;
+
+/**
+ * Created by loulo on 02/05/2016.
+ */
+public interface LoginHandler {
+    public void handleJoin(String cmd);
+}
