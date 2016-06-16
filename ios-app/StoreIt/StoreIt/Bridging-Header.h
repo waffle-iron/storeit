37c3ce8c3e582066bf2f7341fd28041dcc352a92
gjura_r <gjura_r@epitech.eu>
2016-06-16 12:09:49 +0200

    ios/#36-feat: add FB SDK and Bridging-Header

---
+++ ios-app/StoreIt/StoreIt/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  StoreIt
+//
+//  Created by Romain Gjura on 16/06/2016.
+//  Copyright © 2016 Romain Gjura. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+#endif /* Bridging_Header_h */
